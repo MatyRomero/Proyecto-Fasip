715bc59e9f5e268bf5d63f7b0b1796981d3a342f
matias romero <maty@MacBook-Air-de-matias.local>
2022-09-22 08:56:06 -0300

    formulario

--- Proyecto/assets/inc/sendemail.php
+++ Proyecto/assets/inc/sendemail.php
@@ -1,44 +1,40 @@
 <?php
-
-// Define some constants
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "mail@mail.com" );
-
-// Read the form values
-$success = false;
-$name = isset( $_POST['name'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['name'] ) : "";
-$senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
-$phone = isset( $_POST['phone'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['phone'] ) : "";
-$services = isset( $_POST['services'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['services'] ) : "";
-$subject = isset( $_POST['subject'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['subject'] ) : "";
-$address = isset( $_POST['address'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['address'] ) : "";
-$website = isset( $_POST['website'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['website'] ) : "";
-$message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
-
-$mail_subject = 'A contact request send by ' . $name;
-
-$body = 'Name: '. $name . "\r\n";
-$body .= 'Email: '. $senderEmail . "\r\n";
-
-
-if ($phone) {$body .= 'Phone: '. $phone . "\r\n"; }
-if ($services) {$body .= 'services: '. $services . "\r\n"; }
-if ($subject) {$body .= 'Subject: '. $subject . "\r\n"; }
-if ($address) {$body .= 'Address: '. $address . "\r\n"; }
-if ($website) {$body .= 'Website: '. $website . "\r\n"; }
-
-$body .= 'message: ' . "\r\n" . $message;
-
-
-
-// If all values exist, send the email
-if ( $name && $senderEmail && $message ) {
-  $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
-  $headers = "From: " . $name . " <" . $senderEmail . ">";  
-  $success = mail( $recipient, $mail_subject, $body, $headers );
-  echo "<div class='inner success'><p class='success'>Thanks for contacting us. We will contact you ASAP!</p></div><!-- /.inner -->";
-}else {
-	echo "<div class='inner error'><p class='error'>Something went wrong. Please try again.</p></div><!-- /.inner -->";
-}
-
+$remitente = $_POST ['email'];
+$destinatario = 'ignacioquezada@fasipchile.com';
+$asunto = 'Asunto de ejemplo';
+if (!$_POST){
 ?>
+<?php
+}else{
+
+    $cuerpo = "Nombre: " . $_POST["nombre"] . "\r\n";
+    $cuerpo .= "Asunto: " . $_POST["asunto"] . "\r\n";
+    $cuerpo = "Correo: " . $_POST["correo"] . "\r\n";
+    $cuerpo = "Mensaje: " . $_POST["mensaje"] . "\r\n";
+
+
+    $header = "MIME-version: 1.0\n";
+    $header . = "Content-type: text/plain; charset-utf-8\n";
+    $header . = "X-Priority: 3\n";
+    $header . = "X-Mailer: php\n";
+    $header . = "From: \ "".$_POST['nombre']." ".$POST['apellido']."\" <".$remitente.">\n"
+
+    mail($destinatario, $asunto, $cuerpo, $header);
+}
+?>
+
+// <!-- if (isset($_POST['enviar'])) {
+//     if (!empty($_POST['name']) && !empty($_POST['asunto']) && !empty ($_POST['message']) && !empty($_POST['email'])) {
+//         $name = $_POST['name'];
+//         $asunto = $_POST['asunto'];
+//         $message = $_POST['message'];
+//         $email = $_POST['email'];
+//         $header = "From: noreply@example.com" . "\r\n";
+//         $header. = "Reply-To: noreply@example.com" . "\r\n";
+//         $header. =  "X-Mailer: PHP/". phpversion();
+//         $mail = @mail($email,$asunto,$message,$header);
+//         if (mail) {
+//           echo "<h4>!Mail enviado exitosamente!</h4>";
+//         }
+//     }
+// } -->
